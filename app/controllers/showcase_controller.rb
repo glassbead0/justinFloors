f0078db9835418853a96cc09387a695993c20c63
Aaron Glasenapp <glassbead@gmail.com>
2014-08-04 13:01:22 -0600

    panels on home and showcase pages

--- app/controllers/showcase_controller.rb
+++ app/controllers/showcase_controller.rb
@@ -1,12 +1,8 @@
 class ShowcaseController < ApplicationController
   def index
     # @photos = Dir.glob(Rails.root.join('app', 'assets', 'images', '*.JPG'))
-    @photos_wood = [{title: 'wood_2', url: 'wood_2.jpg' }, {title: 'wood_3', url: 'wood_3_stairs.jpg'}, {title: 'wood_4', url: 'wood_4_stairs.jpg'}, {title: 'photo', url: 'wood_5_kitchen.jpg' }, {title: 'photo', url: 'wood_6_kitchen.jpg'}, {title: 'photo', url: 'wood_7.jpg'}, {title: 'photo', url: 'wood_1.jpg' }]
-    @photos_carpet = [{title: 'photo', url: 'carpet_1.jpg' }, {title: 'photo', url: 'carpet_2.jpg' }, {title: 'photo', url: 'carpet_3.jpg' },]
-  end
-
-  def show
-    # @image =
+    @photos_wood = [{title: 'wood_2', url: 'wood_2_thumb.jpg' }, {title: 'wood_3', url: 'wood_3_stairs_thumb.jpg'}, {title: 'wood_4', url: 'wood_4_stairs_thumb.jpg'}, {title: 'photo', url: 'wood_5_kitchen_thumb.jpg' }, {title: 'photo', url: 'wood_6_kitchen_thumb.jpg'}, {title: 'photo', url: 'wood_7_thumb.jpg'}, {title: 'photo', url: 'wood_1_thumb.jpg' }]
+    @photos_carpet = [{title: 'photo', url: 'carpet_1_thumb.jpg' }, {title: 'photo', url: 'carpet_2_thumb.jpg' }, {title: 'photo', url: 'carpet_3_thumb.jpg' },]
   end
 
 end
