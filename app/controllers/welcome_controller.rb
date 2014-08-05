a52ecff13efb15e60180f79400b30c5340375f5a
Aaron Glasenapp <glassbead@gmail.com>
2014-08-05 16:05:37 -0600

    fixed problem with random testimonial generation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,20 +2,24 @@ class WelcomeController < ApplicationController
 
   def index
     @testimonials = Testimonial.all
-    while true do
+    counter = 0
+    while counter < 20 do
       rand1 = rand(@testimonials.length)
       if !@testimonials[rand1].nil?
         @sample_testimonial_1 = @testimonials[rand1]
         break
       end
+      counter += 1
     end
 
-    while true do
+    counter = 0
+    while counter < 20 do
       rand2 = rand(@testimonials.length)
       if !@testimonials[rand2].nil? && rand1 != rand2
         @sample_testimonial_2 = @testimonials[rand2]
         break
       end
+      counter += 1
     end
   end
 
